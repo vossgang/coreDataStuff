0dab97e60fe906347e512145019f45fb57517fc0
vossgang <vossgang@gmail.com>
2014-05-07 09:25:40 -0700

    Initial Commit

---
+++ CoreDataTestTests/CoreDataTestTests.m
@@ -0,0 +1,34 @@
+//
+//  CoreDataTestTests.m
+//  CoreDataTestTests
+//
+//  Created by Matthew Voss on 5/7/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CoreDataTestTests : XCTestCase
+
+@end
+
+@implementation CoreDataTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
